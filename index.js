7ab9a0a205496a032f5d1b22a134c4f07b9a8f1d
seannyandusi <seanmotanya@gmail.com>
2023-09-17 13:37:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function countDown(integer)
+{
+do{console.log(integer)
+  --integer;
+} 
+while(
+  integer >= 0);
+  
+
+}
+
+function  writeCards(array){
+  const cards = [];
+for (let name =0; name < array.length; ++name){
+cards.push(`Thank you, ${array[name]}, for the wonderful surprise gift!`);
+
+}
+  return cards;
+}
